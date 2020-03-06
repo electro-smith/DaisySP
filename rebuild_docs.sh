c6e95031a225674ae641ebe0e0a1b973ae7bf048
stephen hensley <shensley.audio@gmail.com>
2020-03-06 12:18:19 -0800

    new docs.

--- rebuild_docs.sh
+++ rebuild_docs.sh
@@ -30,7 +30,7 @@ echo "done."
 
 mkdir -p doc
 ## daisysp
-pdf_name=doc/daisysp.pdf
+pdf_name=doc/daisysp_reference.pdf
 cat resources/preamble.md > temp.md
 echo "\pagebreak" >> temp.md
 for file in $daisysp_dir/doc/*.md; do
@@ -44,5 +44,9 @@ echo "Created $pdf_name"
 # remove temp file.
 rm temp.md
 echo "done."
+echo "Creating Style Guide PDF"
+pdf_name=doc/style_guide.pdf
+pandoc ./resources/Style-Guide.md  -s --template=./resources/template.tex --toc -o $pdf_name
 echo "finished."
+
 
