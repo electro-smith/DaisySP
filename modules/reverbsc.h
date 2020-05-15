f1b71716abad8d01cff74bd248b7312373f12ada
Stephen Hensley <stephen.p.hensley@gmail.com>
2020-05-15 15:25:43 -0700

    reformatted files and removed commented out redundant code.

--- modules/reverbsc.h
+++ modules/reverbsc.h
@@ -1,5 +1,5 @@
 // # ReverbSc
-// 
+//
 //     Stereo Reverb
 //
 // Ported from soundpipe
@@ -16,69 +16,67 @@
 
 namespace daisysp
 {
-
 typedef struct
 {
-	int     write_pos;
-	int     buffer_size;
-	int     read_pos;
-	int     read_pos_frac;
-	int     read_pos_frac_inc;
-	int     dummy;
-	int     seed_val;
-	int     rand_line_cnt;
-	float filter_state;
-	float *buf;
-}ReverbScDl;
+    int    write_pos;
+    int    buffer_size;
+    int    read_pos;
+    int    read_pos_frac;
+    int    read_pos_frac_inc;
+    int    dummy;
+    int    seed_val;
+    int    rand_line_cnt;
+    float  filter_state;
+    float *buf;
+} ReverbScDl;
 
 class ReverbSc
 {
-    public:
-        ReverbSc() {}
-        ~ReverbSc() {}
-// ### Init
-// Initializes the reverb module, and sets the sample_rate at which the Process function will be called.
-// ~~~~
+  public:
+    ReverbSc() {}
+    ~ReverbSc() {}
+    // ### Init
+    // Initializes the reverb module, and sets the sample_rate at which the Process function will be called.
+    // ~~~~
     int Init(float sample_rate);
-// ~~~~
-    
-// ### Process
-// Process the input through the reverb, and updates values of out1, and out2 with the new processed signal.
-// ~~~~
+    // ~~~~
+
+    // ### Process
+    // Process the input through the reverb, and updates values of out1, and out2 with the new processed signal.
+    // ~~~~
     int Process(const float &in1, const float &in2, float *out1, float *out2);
-// ~~~~
+    // ~~~~
 
-// ### SetFeedback
-// controls the reverb time. reverb tail becomes infinite when set to 1.0
-// 
-// range: 0.0 to 1.0
-//
-// ~~~~
+    // ### SetFeedback
+    // controls the reverb time. reverb tail becomes infinite when set to 1.0
+    //
+    // range: 0.0 to 1.0
+    //
+    // ~~~~
     inline void SetFeedback(const float &fb) { feedback_ = fb; }
-// ~~~~
-// ### SetLpFreq
-// controls the internal dampening filter's cutoff frequency.
-// 
-// range: 0.0 to sample_rate / 2
-//
-// ~~~~
+    // ~~~~
+    // ### SetLpFreq
+    // controls the internal dampening filter's cutoff frequency.
+    //
+    // range: 0.0 to sample_rate / 2
+    //
+    // ~~~~
     inline void SetLpFreq(const float &freq) { lpfreq_ = freq; }
-// ~~~~
+    // ~~~~
 
-    private:
-        void NextRandomLineseg(ReverbScDl *lp, int n);
-        int InitDelayLine(ReverbScDl *lp, int n);
-        float feedback_, lpfreq_;
-        float i_sample_rate_, i_pitch_mod_, i_skip_init_;
-        float sample_rate_;
-        float damp_fact_;
-        float prv_lpfreq_;
-        int init_done_;
-        ReverbScDl delay_lines_[8];
-        float aux_[DSY_REVERBSC_MAX_SIZE];
+  private:
+    void       NextRandomLineseg(ReverbScDl *lp, int n);
+    int        InitDelayLine(ReverbScDl *lp, int n);
+    float      feedback_, lpfreq_;
+    float      i_sample_rate_, i_pitch_mod_, i_skip_init_;
+    float      sample_rate_;
+    float      damp_fact_;
+    float      prv_lpfreq_;
+    int        init_done_;
+    ReverbScDl delay_lines_[8];
+    float      aux_[DSY_REVERBSC_MAX_SIZE];
 };
 
 
 } // namespace daisysp
 #endif
-
