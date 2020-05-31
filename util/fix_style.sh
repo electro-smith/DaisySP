181bee1ef2a939f9c2abcf34517abf4d10c23075
stephenhensley <shensley.audio@gmail.com>
2020-05-30 19:48:39 -0700

    added first version of a fix style script

---
+++ util/fix_style.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#TODO:
+# - fix all the hard-coding
+STARTDIR=`pwd`
+find $STARTDIR -iname '*.h' -type f -exec sed -i 's/\t/    /g' {} +
+find $STARTDIR -iname '*.cpp' -type f -exec sed -i 's/\t/    /g' {} +
+clang-format -i $STARTDIR/modules/*.cpp $STARTDIR/modules/*.h
+clang-format -i $STARTDIR/examples/*/*.cpp $STARTDIR/examples/*/*.h
