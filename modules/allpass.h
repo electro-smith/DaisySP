7db3a043b9d889f087185204645acdb937491758
beserge <bensergentanis@gmail.com>
2020-06-03 12:01:33 -0400

    doc fies

--- modules/allpass.h
+++ modules/allpass.h
@@ -7,46 +7,44 @@
 
 #include <math.h>
 
+/** @file allpass.h */
+
 namespace daisysp
 {
 /**  
-       Allpass filter module
-       
-       Passes all frequencies at their original levels, with a phase shift.
-       
-       Original author(s) : Barry Vercoe, John ffitch
-       Year : 1991
-       Ported from soundpipe by Ben Sergentanis, May 2020
-  */
+       Allpass filter module \n 
+       Passes all frequencies at their original levels, with a phase shift. \n 
+       Ported from soundpipe by Ben Sergentanis, May 2020 
+       @author Barry Vercoe, John ffitch
+       @date 1991
+*/
 class Allpass
 {
   public:
     Allpass() {}
     ~Allpass() {}
 
-    /** Init
-        
+    /**         
         Initializes the allpass module.
         \param sample_rate  The sample rate of the audio engine being run.
+    \param buff Buffer for allpass to use.
+    \param size Size of buff.
     */
     void Init(float sample_rate, float* buff, size_t size);
 
-    /**  Process
-     Returns next sample.
+    /** 
      \param in Input sample.
      \return Next floating point sample.
     */
     float Process(float in);
-    // ~~~~
 
-    /** SetFreq
+    /**
        Sets the filter frequency (Implemented by delay time).
        \param looptime Filter looptime in seconds.
     */
     void SetFreq(float looptime);
 
-    /** SetRevTime
-        Sets the reverberation time in seconds.
+    /**
         \param revtime Reverb time in seconds.
     */
     inline void SetRevTime(float revtime) { rev_time_ = revtime; }
