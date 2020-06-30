ee23523adf8e070b2aa06429da36ddc2a4c190fd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-30 05:00:14 +0000

    Deploy electro-smith/DaisySP to github.com/electro-smith/DaisySP.git:gh-pages

--- search/all_e.js
+++ search/all_e.js
@@ -5,7 +5,7 @@ var searchData=
   ['read_5fpos',['read_pos',['../structdaisysp_1_1_reverb_sc_dl.html#a182f5ab78a0832af510e1ed94c147711',1,'daisysp::ReverbScDl']]],
   ['read_5fpos_5ffrac',['read_pos_frac',['../structdaisysp_1_1_reverb_sc_dl.html#a720c247395a4d03d2e1e6f128e9d2c74',1,'daisysp::ReverbScDl']]],
   ['read_5fpos_5ffrac_5finc',['read_pos_frac_inc',['../structdaisysp_1_1_reverb_sc_dl.html#a75392f0563eb7f8653d098b2bf9d3709',1,'daisysp::ReverbScDl']]],
-  ['reset',['Reset',['../classdaisysp_1_1_delay_line.html#a94bd211eb28ec2bf7cc4a3b1aa12816a',1,'daisysp::DelayLine::Reset()'],['../classdaisysp_1_1_metro.html#a8771aabed09f231ba97cf7a4f7736a53',1,'daisysp::Metro::Reset()'],['../classdaisysp_1_1_oscillator.html#a7e15e57ee6ade915737273329c9216ff',1,'daisysp::Oscillator::Reset()']]],
+  ['reset',['Reset',['../classdaisysp_1_1_bl_osc.html#a52531ea2a4258980106074359043803c',1,'daisysp::BlOsc::Reset()'],['../classdaisysp_1_1_delay_line.html#a94bd211eb28ec2bf7cc4a3b1aa12816a',1,'daisysp::DelayLine::Reset()'],['../classdaisysp_1_1_metro.html#a8771aabed09f231ba97cf7a4f7736a53',1,'daisysp::Metro::Reset()'],['../classdaisysp_1_1_oscillator.html#a7e15e57ee6ade915737273329c9216ff',1,'daisysp::Oscillator::Reset()']]],
   ['reverbsc',['ReverbSc',['../classdaisysp_1_1_reverb_sc.html',1,'daisysp']]],
   ['reverbscdl',['ReverbScDl',['../structdaisysp_1_1_reverb_sc_dl.html',1,'daisysp']]]
 ];
--- search/functions_8.js
+++ search/functions_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['read',['Read',['../classdaisysp_1_1_delay_line.html#a196aed54c4c0a5025d6d82b6816134d3',1,'daisysp::DelayLine']]],
-  ['reset',['Reset',['../classdaisysp_1_1_delay_line.html#a94bd211eb28ec2bf7cc4a3b1aa12816a',1,'daisysp::DelayLine::Reset()'],['../classdaisysp_1_1_metro.html#a8771aabed09f231ba97cf7a4f7736a53',1,'daisysp::Metro::Reset()'],['../classdaisysp_1_1_oscillator.html#a7e15e57ee6ade915737273329c9216ff',1,'daisysp::Oscillator::Reset()']]]
+  ['reset',['Reset',['../classdaisysp_1_1_bl_osc.html#a52531ea2a4258980106074359043803c',1,'daisysp::BlOsc::Reset()'],['../classdaisysp_1_1_delay_line.html#a94bd211eb28ec2bf7cc4a3b1aa12816a',1,'daisysp::DelayLine::Reset()'],['../classdaisysp_1_1_metro.html#a8771aabed09f231ba97cf7a4f7736a53',1,'daisysp::Metro::Reset()'],['../classdaisysp_1_1_oscillator.html#a7e15e57ee6ade915737273329c9216ff',1,'daisysp::Oscillator::Reset()']]]
 ];
