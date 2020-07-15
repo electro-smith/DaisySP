e6ef7333c6b3bef0f3b8e0c4d2564bb63e34b1c0
Stephen Hensley <stephen.p.hensley@gmail.com>
2020-07-15 12:06:50 -0700

    added some faster math functions to compressor, and updated vcxproj with latest modules

--- modules/compressor.cpp
+++ modules/compressor.cpp
@@ -25,6 +25,28 @@ using namespace daisysp;
 #define min(a, b) ((a < b) ? a : b)
 #endif
 
+
+// This function and the two macros below come from:
+// http://openaudio.blogspot.com/2017/02/faster-log10-and-pow.html
+static float log2f_approx(float X)
+{
+    float Y, F;
+    int   E;
+    F = frexpf(fabsf(X), &E);
+    Y = 1.23149591368684f;
+    Y *= F;
+    Y += -4.11852516267426f;
+    Y *= F;
+    Y += 6.02197014179219f;
+    Y *= F;
+    Y += -3.13396450166353f;
+    Y += E;
+    return (Y);
+}
+
+#define pow10f(x) expf(2.302585092994046f * x)
+#define log10f_fast(x) (log2f_approx(x) * 0.3010299956639812f)
+
 void Compressor::Init(float sample_rate)
 {
     sr_       = sample_rate;
@@ -54,10 +76,14 @@ float Compressor::Process(float in, float key)
     float f_temp2 = ((f_rec1_[1] > f_temp1) ? f_slow4_ : f_slow3_);
     f_rec2_[0]    = ((f_rec2_[1] * f_temp2) + ((1.0f - f_temp2) * f_temp1));
     f_rec1_[0]    = f_rec2_[0];
-    f_rec0_[0]    = ((f_slow1_ * f_rec0_[1])
-                  + (f_slow2_
-                     * max(((20.f * std::log10(f_rec1_[0])) - f_slow5_), 0.f)));
-    out = (powf(10.0f, (0.05f * f_rec0_[0] * (0.05f * makeup_gain_))) * in);
+    f_rec0_[0]
+        = ((f_slow1_ * f_rec0_[1])
+           + (f_slow2_ * max(((20.f * log10f_fast(f_rec1_[0])) - f_slow5_), 0.f)));
+//    f_rec0_[0]
+//        = ((f_slow1_ * f_rec0_[1])
+//           + (f_slow2_ * max(((20.f * log10f(f_rec1_[0])) - f_slow5_), 0.f)));
+    //out = (powf(10.0f, (0.05f * f_rec0_[0] * (0.05f * makeup_gain_))) * in);
+    out        = pow10f(0.05f * f_rec0_[0]) * in;
     f_rec2_[1] = f_rec2_[0];
     f_rec1_[1] = f_rec1_[0];
     f_rec0_[1] = f_rec0_[0];
@@ -72,7 +98,8 @@ float Compressor::Process(float in)
 
 void Compressor::RecalculateSlopes()
 {
-    makeup_gain_ = fabsf(thresh_ / ratio_) / 2.0f;
+    //makeup_gain_ = fabsf(thresh_ / ratio_) / 2.0f;
+    makeup_gain_ = 1.0f;
     f_slow0_     = (float)atk_; // probably need to scale
     f_slow1_     = expf(0.0f - (f_const1_ / f_slow0_));
     f_slow2_     = ((1.0f - f_slow1_) * ((1.0f / ratio_) - 1.0f));
