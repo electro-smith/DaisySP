afb8d7f793dd0ba2b17021f4880f9610aae0a53b
U-MSI\Ben <bensergentanis@gmail.com>
2020-12-22 13:38:21 -0500

    even more formanty

--- examples/formantosc/ex_formantosc.cpp
+++ examples/formantosc/ex_formantosc.cpp
@@ -7,18 +7,14 @@ using namespace daisy;
 static DaisySeed  seed;
 static FormantOscillator  form;
 static Oscillator lfo;
-static Oscillator osc;
 
 static void AudioCallback(float *in, float *out, size_t size)
 {
   for(size_t i = 0; i < size; i += 2)
     {
-		float sig = 500.f * fabsf(lfo.Process());
+		float sig = 800.f *  fabsf(lfo.Process()) + 200;
 		form.SetFormantFreq(sig);
-		
-		float o = (osc.Process() + 1) * .5f;
-		form.SetPhaseShift(o * .4);
-		
+				
 		out[i] = out[i + 1] = form.Process();
     }
 }
@@ -37,10 +33,6 @@ int main(void)
 	lfo.Init(sample_rate);
 	lfo.SetAmp(1.f);
 	lfo.SetFreq(.25f);
-
-	osc.Init(sample_rate);
-	osc.SetAmp(1.f);
-	osc.SetFreq(100.f);
 
     // start callback
     seed.StartAudio(AudioCallback);
