08547db4c1e1da25dd78e32823f006e0d45d73bd
U-MSI\Ben <bensergentanis@gmail.com>
2020-12-22 17:34:27 -0500

    phase shift

--- examples/formantosc/ex_formantosc.cpp
+++ examples/formantosc/ex_formantosc.cpp
@@ -7,6 +7,7 @@ using namespace daisy;
 static DaisySeed         seed;
 static FormantOscillator form[2];
 static Metro             tick;
+static Oscillator        lfo;
 
 float freqs[3][2] = {{390, 2300}, {610, 1900}, {820, 1530}};
 int   idx         = 0;
@@ -19,6 +20,9 @@ static void AudioCallback(float *in, float *out, size_t size)
         {
             idx = (idx + 1) % 3;
         }
+
+        form[0].SetPhaseShift(lfo.Process());
+        form[1].SetPhaseShift(lfo.Process());
 
         form[0].SetFormantFreq(freqs[idx][0]);
         form[1].SetFormantFreq(freqs[idx][1]);
@@ -40,6 +44,10 @@ int main(void)
 
     form[1].Init(sample_rate);
     form[1].SetCarrierFreq(200.f);
+
+    lfo.Init(sample_rate);
+    lfo.SetAmp(1.f);
+    lfo.SetFreq(.2f);
 
     tick.Init(1.f, sample_rate);
 
