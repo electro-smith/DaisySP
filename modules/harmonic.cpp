545e2236dd8f0ef96dabca2bcab31d1704a66b91
U-MSI\Ben <bensergentanis@gmail.com>
2020-12-28 13:16:39 -0500

    working block based

--- modules/harmonic.cpp
+++ modules/harmonic.cpp
@@ -3,59 +3,64 @@
 
 using namespace daisysp;
 
-  void HarmonicOscillator::Init(int num_harmonics) {
-    phase_ = 0.0f;
-    frequency_ = 0.0f;
-	num_harmonics_ = num_harmonics > 32 ? 32 : num_harmonics;
-    for (int i = 0; i < num_harmonics_; ++i) {
-      amplitude_[i] = 0.0f;
+void HarmonicOscillator::Init(int num_harmonics)
+{
+    phase_         = 0.0f;
+    frequency_     = 0.0f;
+    num_harmonics_ = num_harmonics > 32 ? 32 : num_harmonics;
+    for(int i = 0; i < num_harmonics_; ++i)
+    {
+        amplitude_[i] = 0.0f;
     }
-  }
-  
-  void HarmonicOscillator::Process(
-	int first_harmonic_index, 
-  float frequency,
-      const float* amplitudes,
-      float* out,
-      size_t size) {
-    if (frequency >= 0.5f) {
-      frequency = 0.5f;
+}
+
+float HarmonicOscillator::Process(int          first_harmonic_index,
+                                  float        frequency,
+                                  const float* amplitudes)
+{
+    if(frequency >= 0.5f)
+    {
+        frequency = 0.5f;
     }
-        
-    for (int i = 0; i < num_harmonics_; ++i) {
-      float f = frequency * static_cast<float>(first_harmonic_index + i);
-      if (f >= 0.5f) {
-        f = 0.5f;
-      }
-      amplitude_[i] = amplitudes[i] * (1.0f - f * 2.0f);
+	frequency_ = frequency;
+
+    for(int i = 0; i < num_harmonics_; ++i)
+    {
+        float f = frequency * static_cast<float>(first_harmonic_index + i);
+        if(f >= 0.5f)
+        {
+            f = 0.5f;
+        }
+        amplitude_[i] = amplitudes[i] * (1.0f - f * 2.0f);
     }
 
-      phase_ += frequency_;
-      if (phase_ >= 1.0f) {
+    phase_ += frequency_;
+    if(phase_ >= 1.0f)
+    {
         phase_ -= 1.0f;
-      }
-      const float two_x = 2.0f * sinf(phase_ * TWOPI_F);
-      float previous, current;
-      if (first_harmonic_index == 1) {
+    }
+    const float two_x = 2.0f * sinf(phase_ * TWOPI_F);
+    float       previous, current;
+    if(first_harmonic_index == 1)
+    {
         previous = 1.0f;
-        current = two_x * 0.5f;
-      } else {
+        current  = two_x * 0.5f;
+    }
+    else
+    {
         const float k = first_harmonic_index;
-        previous = sinf((phase_ * (k - 1.0f) + 0.25f) * TWOPI_F);
-        current = sinf((phase_ * k) * TWOPI_F);
-      }
-      
-      float sum = 0.0f;
-      for (int i = 0; i < num_harmonics_; ++i) {
+        previous      = sinf((phase_ * (k - 1.0f) + 0.25f) * TWOPI_F);
+        current       = sinf((phase_ * k) * TWOPI_F);
+    }
+
+    float sum = 0.0f;
+    for(int i = 0; i < num_harmonics_; ++i)
+    {
         sum += amplitude_[i] * current;
         float temp = current;
-        current = two_x * current - previous;
-        previous = temp;
-      }
-      if (first_harmonic_index == 1) {
-        *out++ = sum;
-      } else {
-        *out++ += sum;
-      }
-	
-	}
+        current    = two_x * current - previous;
+        previous   = temp;
+    }
+
+    return sum;
+}
--- modules/harmonic.h
+++ modules/harmonic.h
@@ -6,8 +6,8 @@
 #ifdef __cplusplus
 
 /** @file harmonic.h */
-namespace daisysp {
-
+namespace daisysp
+{
 /**  
        Harmonic Oscillator Module based on Chebyshev polynomials \n 
 	   Works well for a small number of harmonics. For the higher order harmonics. \n
@@ -16,23 +16,25 @@ namespace daisysp {
        @author Emilie Gillet
        @date 2016
 */
-class HarmonicOscillator {
- public:
-  HarmonicOscillator() {}
-  ~HarmonicOscillator() {}
+class HarmonicOscillator
+{
+  public:
+    HarmonicOscillator() {}
+    ~HarmonicOscillator() {}
 
-  void Init(int num_harmonics);
-  void Process(int first_harmonic_index, float frequency, const float* amplitudes, float* out, size_t size);
+    void Init(int num_harmonics);
+    float
+    Process(int first_harmonic_index, float frequency, const float* amplitudes);
 
- private:
-	int num_harmonics_;
-  // Oscillator state.
-  float phase_;
+  private:
+    int num_harmonics_;
+    // Oscillator state.
+    float phase_;
 
-  // For interpolation of parameters.
-  float frequency_;
-  float amplitude_[32];
+    // For interpolation of parameters.
+    float frequency_;
+    float amplitude_[32];
 };
-}  // namespace daisysp
+} // namespace daisysp
 #endif
 #endif
