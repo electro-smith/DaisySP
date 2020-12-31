f3c78dbe50f162da36287e774beeb54da2e4e26c
U-MSI\Ben <bensergentanis@gmail.com>
2020-12-31 15:02:54 -0500

    setters v1

--- modules/zoscillator.cpp
+++ modules/zoscillator.cpp
@@ -4,107 +4,115 @@
 
 using namespace daisysp;
 
-  void ZOscillator::Init(float sample_rate) {
-	sample_rate_ = sample_rate;
+void ZOscillator::Init(float sample_rate)
+{
+    sample_rate_ = sample_rate;
 
-    carrier_phase_ = 0.0f;
+    carrier_phase_       = 0.0f;
     discontinuity_phase_ = 0.0f;
-    formant_phase_ = 0.0f;
-    next_sample_ = 0.0f;
-  
+    formant_phase_       = 0.0f;
+    next_sample_         = 0.0f;
+
     carrier_frequency_ = 0.0f;
     formant_frequency_ = 0.0f;
-    carrier_shape_ = 0.0f;
-    mode_ = 0.0f;
-  }
-  
-  float ZOscillator::Process(
-      float carrier_frequency,
-      float formant_frequency,
-      float carrier_shape,
-      float mode) {
+    carrier_shape_     = 0.0f;
+    mode_              = 0.0f;
+}
 
-	float kMaxFrequency = .25f;
-		  
-    if (carrier_frequency >= kMaxFrequency * 0.5f) {
-      carrier_frequency = kMaxFrequency * 0.5f;
-    }
-    if (formant_frequency >= kMaxFrequency) {
-      formant_frequency = kMaxFrequency;
-    }
-    
+float ZOscillator::Process(){
     float next_sample = next_sample_;
-    
-      bool reset = false;
-      float reset_time = 0.0f;
 
-      float this_sample = next_sample;
-      next_sample = 0.0f;
+    bool  reset      = false;
+    float reset_time = 0.0f;
 
-	carrier_frequency_ = carrier_frequency;
-	formant_frequency_ = formant_frequency;
-    
-      discontinuity_phase_ += 2.0f * carrier_frequency_;
-      carrier_phase_ += carrier_frequency_;
-      reset = discontinuity_phase_ >= 1.0f;
-      
-      if (reset) {
+    float this_sample = next_sample;
+    next_sample       = 0.0f;
+
+    discontinuity_phase_ += 2.0f * carrier_frequency_;
+    carrier_phase_ += carrier_frequency_;
+    reset = discontinuity_phase_ >= 1.0f;
+
+    if(reset)
+    {
         discontinuity_phase_ -= 1.0f;
         reset_time = discontinuity_phase_ / (2.0f * carrier_frequency_);
-        
-        float carrier_phase_before = carrier_phase_ >= 1.0f ? 1.0f : 0.5f;
-        float carrier_phase_after = carrier_phase_ >= 1.0f ? 0.0f : 0.5f;
-		
-		float mode_sub = mode_ + (1.f - reset_time) * (mode_ - mode);
-		float shape_sub = carrier_shape_ + (1.0f - reset_time) * (carrier_shape_ - carrier_shape);
-        float before = Z(
-            carrier_phase_before,
-            1.0f,
-            formant_phase_ + (1.0f - reset_time) * formant_frequency_,
-            shape_sub, mode_sub);
 
-        float after = Z(
-            carrier_phase_after,
-            0.0f,
-            0.0f,
-            carrier_shape,
-            mode);
+        float carrier_phase_before = carrier_phase_ >= 1.0f ? 1.0f : 0.5f;
+        float carrier_phase_after  = carrier_phase_ >= 1.0f ? 0.0f : 0.5f;
+
+        float mode_sub  = mode_ + (1.f - reset_time) * (mode_ - mode_new_);
+        float shape_sub = carrier_shape_ + (1.0f - reset_time) * (carrier_shape_ - shape_new_);
+        float before
+            = Z(carrier_phase_before,
+                1.0f,
+                formant_phase_ + (1.0f - reset_time) * formant_frequency_,
+                shape_sub,
+                mode_sub);
+
+        float after = Z(carrier_phase_after, 0.0f, 0.0f, shape_new_, mode_new_);
 
         float discontinuity = after - before;
         this_sample += discontinuity * ThisBlepSample(reset_time);
         next_sample += discontinuity * NextBlepSample(reset_time);
         formant_phase_ = reset_time * formant_frequency_;
-        
-        if (carrier_phase_ > 1.0f) {
-          carrier_phase_ = discontinuity_phase_ * 0.5f;
-        }
-      } else {
-        formant_phase_ += formant_frequency_;
-        if (formant_phase_ >= 1.0f) {
-          formant_phase_ -= 1.0f;
-        }
-      }
-      
-      if (carrier_phase_ >= 1.0f) {
-        carrier_phase_ -= 1.0f;
-      }
-      
-	  carrier_shape_ = carrier_shape;
-	  mode_ = mode;
-      next_sample += Z(
-          carrier_phase_,
-          discontinuity_phase_,
-          formant_phase_,
-		  carrier_shape_,
-		  mode_);
-		  
-    next_sample_ = next_sample;
-	return this_sample;
-  }
 
-  inline float ZOscillator::Sine(float phase) {
+        if(carrier_phase_ > 1.0f)
+        {
+            carrier_phase_ = discontinuity_phase_ * 0.5f;
+        }
+    }
+    else
+    {
+        formant_phase_ += formant_frequency_;
+        if(formant_phase_ >= 1.0f)
+        {
+            formant_phase_ -= 1.0f;
+        }
+    }
+
+    if(carrier_phase_ >= 1.0f)
+    {
+        carrier_phase_ -= 1.0f;
+    }
+
+    carrier_shape_ = shape_new_;
+    mode_          = mode_new_;
+    next_sample += Z(carrier_phase_,
+                     discontinuity_phase_,
+                     formant_phase_,
+                     carrier_shape_,
+                     mode_);
+
+    next_sample_ = next_sample;
+    return this_sample;
+}
+
+inline float ZOscillator::Sine(float phase)
+{
     return sinf(phase * TWOPI_F);
-  }
+}
+
+void ZOscillator::SetFormantFreq(float freq)
+{
+    //convert from Hz to phase_inc / sample
+    formant_frequency_ = freq / sample_rate_;
+    formant_frequency_ = formant_frequency_ >= .25f ? .25f : formant_frequency_;
+}
+
+void ZOscillator::SetCarrierFreq(float freq)
+{
+    //convert from Hz to phase_inc / sample
+    carrier_frequency_ = freq / sample_rate_;
+    carrier_frequency_ = carrier_frequency_ >= .25f ? .25f : carrier_frequency_;
+}
+
+void ZOscillator::SetShape(float shape){
+	shape_new_ = shape;
+}
+
+void ZOscillator::SetMode(float mode){
+	mode_new_ = mode;
+}
 
 inline float ZOscillator::ThisBlepSample(float t)
 {
@@ -117,32 +125,42 @@ inline float ZOscillator::NextBlepSample(float t)
     return -0.5f * t * t;
 }
 
-  inline float ZOscillator::Z(float c, float d, float f, float shape, float mode) {
+inline float ZOscillator::Z(float c, float d, float f, float shape, float mode)
+{
     float ramp_down = 0.5f * (1.0f + Sine(0.5f * d + 0.25f));
-    
+
     float offset;
     float phase_shift;
-    if (mode < 0.333f) {
-      offset = 1.0f;
-      phase_shift = 0.25f + mode * 1.50f;
-    } else if (mode < 0.666f) {
-      phase_shift = 0.7495f - (mode - 0.33f) * 0.75f;
-      offset = -Sine(phase_shift);
-    } else {
-      phase_shift = 0.7495f - (mode - 0.33f) * 0.75f;
-      offset = 0.001f;
+    if(mode < 0.333f)
+    {
+        offset      = 1.0f;
+        phase_shift = 0.25f + mode * 1.50f;
     }
-    
+    else if(mode < 0.666f)
+    {
+        phase_shift = 0.7495f - (mode - 0.33f) * 0.75f;
+        offset      = -Sine(phase_shift);
+    }
+    else
+    {
+        phase_shift = 0.7495f - (mode - 0.33f) * 0.75f;
+        offset      = 0.001f;
+    }
+
     float discontinuity = Sine(f + phase_shift);
     float contour;
-    if (shape < 0.5f) {
-      shape *= 2.0f;
-      if (c >= 0.5f) {
-        ramp_down *= shape;
-      }
-      contour = 1.0f + (Sine(c + 0.25f) - 1.0f) * shape;
-    } else {
-      contour = Sine(c + shape * 0.5f);
+    if(shape < 0.5f)
+    {
+        shape *= 2.0f;
+        if(c >= 0.5f)
+        {
+            ramp_down *= shape;
+        }
+        contour = 1.0f + (Sine(c + 0.25f) - 1.0f) * shape;
+    }
+    else
+    {
+        contour = Sine(c + shape * 0.5f);
     }
     return (ramp_down * (offset + discontinuity) - offset) * contour;
-  }
+}
--- modules/zoscillator.h
+++ modules/zoscillator.h
@@ -8,7 +8,6 @@
 /** @file zoscillator.h */
 namespace daisysp
 {
-	
 /**  
     ZOscillator Module \n 
 	Sinewave multiplied by and sync'ed to a carrier. \n
@@ -16,39 +15,40 @@ namespace daisysp
     @author Emilie Gillet
     @date 2016
 */
-class ZOscillator {
- public:
-  ZOscillator() { }
-  ~ZOscillator() { }
+class ZOscillator
+{
+  public:
+    ZOscillator() {}
+    ~ZOscillator() {}
 
-  void Init(float sample_rate);
-  
-  float Process(
-      float carrier_frequency,
-      float formant_frequency,
-      float carrier_shape,
-      float mode);
-	  
- private:
-  inline float Sine(float phase);
-  inline float Z(float c, float d, float f, float shape, float mode);
-  inline float ThisBlepSample(float t);
-  inline float NextBlepSample(float t);
+    void Init(float sample_rate);
 
-	float sample_rate_;
-	
-  // Oscillator state.
-  float carrier_phase_;
-  float discontinuity_phase_;
-  float formant_phase_;
-  float next_sample_;
+    float Process();
 
-  // For interpolation of parameters.
-  float carrier_frequency_;
-  float formant_frequency_;
-  float carrier_shape_;
-  float mode_;
-};  
-}  // namespace daisysp
+  private:
+    inline float Sine(float phase);
+    inline float Z(float c, float d, float f, float shape, float mode);
+    inline float ThisBlepSample(float t);
+    inline float NextBlepSample(float t);
+	void SetFormantFreq(float freq);
+	void SetCarrierFreq(float freq);
+	void SetShape(float shape);
+	void SetMode(float mode);
+
+    float sample_rate_;
+
+    // Oscillator state.
+    float carrier_phase_;
+    float discontinuity_phase_;
+    float formant_phase_;
+    float next_sample_;
+
+    // For interpolation of parameters.
+    float carrier_frequency_;
+    float formant_frequency_;
+    float carrier_shape_, shape_new_;
+    float mode_, mode_new_;
+};
+} // namespace daisysp
 #endif
 #endif
