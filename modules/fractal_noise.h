c4e3c53775e8d13faf3368461c8ebf87479af78e
U-MSI\Ben <bensergentanis@gmail.com>
2021-01-11 12:42:38 -0500

    comments and formatting

--- modules/fractal_noise.h
+++ modules/fractal_noise.h
@@ -1,6 +1,6 @@
 #pragma once
-#ifndef DSY_FORMANTOSCILLATOR_H
-#define DSY_FORMANTOSCILLATOR_H
+#ifndef DSY_FRACTAL_H
+#define DSY_FRACTAL_H
 
 #include <stdint.h>
 #ifdef __cplusplus
@@ -11,6 +11,8 @@ namespace daisysp
 {
 /**  
        Stack octaves of a noise source to generate a fractal noise. \n  
+	   T is the noise source to use. T must have SetFreq() and Init(sample_rate) functions. \n
+	   Order is the number of noise sources to stack. \n
        Ported by Ben Sergentanis, Jan 2021 
        @author Emilie Gillet
        @date 2016
@@ -22,16 +24,22 @@ class FractalRandomGenerator
     FractalRandomGenerator() {}
     ~FractalRandomGenerator() {}
 
-    void Init()
+	/** Initialize the module
+		\param sample_rate Audio engine sample rate.
+	*/
+    void Init(float sample_rate)
     {
-        SetDecay(.5f);
+		sample_rate_ = sample_rate;
+		
+        SetColor(.5f);
         SetFreq(440.f);
         for(int i = 0; i < order; ++i)
         {
-            generator_[i].Init();
+            generator_[i].Init(sample_rate_);
         }
     }
 
+	/** Get the next sample. */
     float Process()
     {
         float gain      = 0.5f;
@@ -40,7 +48,8 @@ class FractalRandomGenerator
 
         for(int i = 0; i < order; ++i)
         {
-            sum += generator_[i].Process(frequency) * gain;
+			generator_[i].SetFreq(frequency);
+            sum += generator_[i].Process() * gain;
             gain *= decay_;
             frequency *= 2.0f;
         }
@@ -48,15 +57,21 @@ class FractalRandomGenerator
         return sum;
     }
 
+	/** Set the lowest noise frequency.
+		\param freq Frequency of the lowest noise source in Hz.
+	*/
     void SetFreq(float freq)
     {
-        freq       = freq / sample_rate_;
-        frequency_ = fclamp(freq, 0.f, 1.f);
+        frequency_ = fclamp(freq, 0.f, sample_rate_);
     }
 
-    void SetDecay(float decay) { decay_ = fclamp(decay, 0.f, 1.f); }
+	/** Sets the amount of high frequency noise.
+		\** Works 0-1. 1 is the brightest, and 0 is the darkest.
+	*/
+    void SetColor(float color) { decay_ = fclamp(color, 0.f, 1.f); }
 
   private:
+	float sample_rate_;
     float frequency_;
     float decay_;
 
