127f54ac69c1090ce1917ad0e7cdb9385f046995
U-MSI\Ben <bensergentanis@gmail.com>
2021-01-11 10:11:01 -0500

    clamp and comment

--- modules/overdrive.cpp
+++ modules/overdrive.cpp
@@ -1,41 +1,54 @@
 #include "dsp.h"
 #include "overdrive.h"
+#include <algorithm>
 
 using namespace daisysp;
 
-  void Overdrive::Init() {
-    pre_gain_ = 0.0f;
+void Overdrive::Init()
+{
+    drive_     = 0.f;
+    pre_gain_  = 0.0f;
     post_gain_ = 0.0f;
-  }
-  
-  float Overdrive::Process(float in) {
-    const float drive_2 = drive_ * drive_;
+}
+
+float Overdrive::Process(float in)
+{
+    const float drive_2    = drive_ * drive_;
     const float pre_gain_a = drive_ * 0.5f;
     const float pre_gain_b = drive_2 * drive_2 * drive_ * 24.0f;
-    const float pre_gain_ = pre_gain_a + (pre_gain_b - pre_gain_a) * drive_2;
+    const float pre_gain_  = pre_gain_a + (pre_gain_b - pre_gain_a) * drive_2;
     const float drive_squashed = drive_ * (2.0f - drive_);
-    const float post_gain_ = 1.0f / SoftClip(
-          0.33f + drive_squashed * (pre_gain_ - 0.33f));
-        
-      float pre = pre_gain_ * in;
-      return SoftClip(pre) * post_gain_;
+    const float post_gain_
+        = 1.0f / SoftClip(0.33f + drive_squashed * (pre_gain_ - 0.33f));
+
+    float pre = pre_gain_ * in;
+    return SoftClip(pre) * post_gain_;
+}
+
+
+void Overdrive::SetDrive(float drive)
+{
+    drive  = DSY_CLAMP(drive, 0, 1);
+    drive_ = 2.f * drive;
+}
+
+inline float Overdrive::SoftLimit(float x)
+{
+    return x * (27.0f + x * x) / (27.0f + 9.0f * x * x);
+}
+
+inline float Overdrive::SoftClip(float x)
+{
+    if(x < -3.0f)
+    {
+        return -1.0f;
     }
-	
-	
-	void Overdrive::SetDrive(float drive){
-		drive_ = drive;
-	}
-	
-  inline float Overdrive::SoftLimit(float x) {
-	return x * (27.0f + x * x) / (27.0f + 9.0f * x * x);
-  }
-	
-  inline float Overdrive::SoftClip(float x) {
-  if (x < -3.0f) {
-    return -1.0f;
-  } else if (x > 3.0f) {
-    return 1.0f;
-  } else {
-    return SoftLimit(x);
-  }
-  }
+    else if(x > 3.0f)
+    {
+        return 1.0f;
+    }
+    else
+    {
+        return SoftLimit(x);
+    }
+}
--- modules/overdrive.h
+++ modules/overdrive.h
@@ -15,26 +15,33 @@ namespace daisysp
        @author Emilie Gillet
        @date 2014
 */
-class Overdrive {
- public:
-  Overdrive() { }
-  ~Overdrive() { }
-  
-  void Init();
-  
-  float Process(float in);
-  
-  void SetDrive(float drive);
-  
- private:
- inline float SoftClip(float x);
- inline float SoftLimit(float x);
- 
-  float drive_;
-  float pre_gain_;
-  float post_gain_;
-  
+class Overdrive
+{
+  public:
+    Overdrive() {}
+    ~Overdrive() {}
+
+    /** Initializes the module with 0 gain */
+    void Init();
+
+    /** Get the next sample
+		\param in Input to be overdriven
+	*/
+    float Process(float in);
+
+    /** Set the amount of drive
+		\param drive Works from 0-1
+	*/
+    void SetDrive(float drive);
+
+  private:
+    inline float SoftClip(float x);
+    inline float SoftLimit(float x);
+
+    float drive_;
+    float pre_gain_;
+    float post_gain_;
 };
-}  // namespace daisysp
+} // namespace daisysp
 #endif
 #endif
