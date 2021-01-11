f24f7f8fe5a0308fc30a7d6f960e3473de02b25f
U-MSI\Ben <bensergentanis@gmail.com>
2021-01-11 11:09:02 -0500

    port

---
+++ modules/smooth_random.h
@@ -0,0 +1,69 @@
+#pragma once
+#ifndef DSY_SMOOTHRANDOM_H
+#define DSY_SMOOTHRANDOM_H
+
+#include dsp.h
+#include <stdint.h>
+#ifdef __cplusplus
+
+/** @file smooth_random.h */
+
+namespace daisysp
+{
+/**  
+		Smooth random generator for the internal modulations \n
+       Ported by Ben Sergentanis, Jan 2021 
+       @author Emilie Gillet
+       @date 2016
+*/
+class SmoothRandomGenerator {
+ public:
+  SmoothRandomGenerator() { }
+  ~SmoothRandomGenerator() { }
+  
+ void Init(float sample_rate) {
+	sample_rate_ = sample_rate;
+	Seed(0x8D5A61A4);
+	
+	SetFreq(1.f);
+    phase_ = 0.0f;
+    from_ = 0.0f;
+    interval_ = 0.0f;
+  }
+  
+  float Process() {
+    phase_ += frequency_;
+    if (phase_ >= 1.0f) {
+      phase_ -= 1.0f;
+      from_ += interval_;
+      interval_ = GetFloat() * 2.0f - 1.0f - from_;
+    }
+    float t = phase_ * phase_ * (3.0f - 2.0f * phase_);
+    return from_ + interval_ * t;
+  }
+
+	void SetFreq(float freq){
+		freq = freq / sample_rate_;
+		frequency_ = fclamp(freq, 0.f, 1.f);
+	}
+
+	float Seed(uint32_t seed){
+		rng_state_ = seed;
+	}
+  
+ private:
+  float frequency_;
+  float phase_;
+  float from_;
+  float interval_;
+  
+  float sample_rate_;
+  
+  //from stmlib random.h
+  inline float GetFloat();
+  int32_t      rng_state_;
+};
+
+}  // namespace daisysp
+#endif
+#endif
