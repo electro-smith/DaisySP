a7251c0cc2d9f942e57d904ce80ec1396a1dc68c
U-MSI\Ben <bensergentanis@gmail.com>
2021-01-11 16:07:42 -0500

    working

--- modules/dust.h
+++ modules/dust.h
@@ -3,6 +3,7 @@
 #define DSY_DUST_H
 
 #include <stdint.h>
+#include <float.h>
 #ifdef __cplusplus
 
 /** @file dust.h */
@@ -11,7 +12,7 @@ namespace daisysp
 {
 /**  
 		Randomly Clocked Samples \n
-       Ported by Ben Sergentanis, Dec 2020 
+       Ported by Ben Sergentanis, Jan 2021
        @author Emilie Gillet
        @date 2016
 */
@@ -21,37 +22,35 @@ class Dust
     Dust() {}
     ~Dust() {}
 
-	void Init(float sample_rate){
-		sample_rate_ = sample_rate;
-		SetFreq(1000.f);
-	}
+    void Init() { SetDensity(.5f); }
 
     float Process()
     {
-        float inv_frequency = 1.0f / frequency_;
-        float u             = GetFloat();
-        if(u < frequency_)
+        float inv_density = 1.0f / density_;
+        float u           = GetFloat();
+        if(u < density_)
         {
-            return u * inv_frequency;
+            return u * inv_density;
         }
         return 0.0f;
     }
 
-	void SetFreq(float freq){
-		freq /= sample_rate_;
-		frequency_ = fclamp(freq, 0.f, 1.f);
-	}
+    void SetDensity(float density)
+    {
+        density_ = fclamp(density, 0.f, 1.f);
+        density_ = -.5f + density_ * .13f;
+    }
 
   private:
-	float frequency_;
-	float sample_rate_;
-	
+    float density_;
+    float frac_ = 1.f / FLT_MAX;
+
     inline float GetFloat()
-	{
-		rng_state_ = rng_state_ * 1664525L + 1013904223L;
-		return static_cast<float>(rng_state_) / 4294967296.0f;
-	}
-    int32_t      rng_state_ = 0x8D5A61A4; //default seed
+    {
+        rng_state_ = rng_state_ * 1664525L + 1013904223L;
+        return static_cast<float>(rng_state_) / 4294967296.0f;
+    }
+    int32_t rng_state_ = 0x8D5A6285; //default seed
 };
 } // namespace daisysp
 #endif
