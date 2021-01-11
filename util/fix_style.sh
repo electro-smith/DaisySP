f04069e0f344ecd86f99fbe725fa276be6aeb09e
stephenhensley <stephen.p.hensley@gmail.com>
2021-01-11 08:30:30 -0800

    fixed fix_style so it doesn't check for examples.

--- util/fix_style.sh
+++ util/fix_style.sh
@@ -5,5 +5,4 @@ STARTDIR=`pwd`
 find $STARTDIR -iname '*.h' -type f -exec sed -i 's/\t/    /g' {} +
 find $STARTDIR -iname '*.cpp' -type f -exec sed -i 's/\t/    /g' {} +
 clang-format -i $STARTDIR/modules/*.cpp $STARTDIR/modules/*.h
-clang-format -i $STARTDIR/examples/*/*.cpp
 
