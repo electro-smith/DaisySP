9ceb2d8f8b46c9edea0d438386eb56073ee5d225
U-MSI\Ben <bensergentanis@gmail.com>
2021-01-12 11:39:18 -0500

    replaced GetFloat

--- modules/dust.h
+++ modules/dust.h
@@ -3,7 +3,6 @@
 #define DSY_DUST_H
 
 #include <stdint.h>
-#include <float.h>
 #ifdef __cplusplus
 
 /** @file dust.h */
@@ -31,7 +30,7 @@ class Dust
     float Process()
     {
         float inv_density = 1.0f / density_;
-        float u           = GetFloat();
+        float u           = rand() * frac_;
         if(u < density_)
         {
             return u * inv_density;
@@ -42,19 +41,12 @@ class Dust
     void SetDensity(float density)
     {
         density_ = fclamp(density, 0.f, 1.f);
-        density_ = -.5f + density_ * .13f;
+        density_ = density_ * .3f;
     }
 
   private:
     float density_;
-    float frac_ = 1.f / FLT_MAX;
-
-    inline float GetFloat()
-    {
-        rng_state_ = rng_state_ * 1664525L + 1013904223L;
-        return static_cast<float>(rng_state_) / 4294967296.0f;
-    }
-    int32_t rng_state_ = 0x8D5A6285; //default seed
+    float frac_ = 1.f / RAND_MAX;
 };
 } // namespace daisysp
 #endif
