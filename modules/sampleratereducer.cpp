331edd513076edf43c51b3259900d2daba323a5e
U-MSI\Ben <bensergentanis@gmail.com>
2021-01-12 13:44:23 -0500

    simplified

--- modules/sampleratereducer.cpp
+++ modules/sampleratereducer.cpp
@@ -5,7 +5,7 @@
 using namespace daisysp;
 void SampleRateReducer::Init()
 {
-    frequency_       = .5f; //half sample rate
+    frequency_       = .2f;
     phase_           = 0.0f;
     sample_          = 0.0f;
     next_sample_     = 0.0f;
@@ -14,35 +14,25 @@ void SampleRateReducer::Init()
 
 float SampleRateReducer::Process(float in)
 {
-    float previous_sample = previous_sample_;
-    float next_sample     = next_sample_;
-    float sample          = sample_;
-    float phase           = phase_;
-
-    float this_sample = next_sample;
-    next_sample       = 0.0f;
-    phase += frequency_;
-	if(phase >= 1.0f)
+    float this_sample = next_sample_;
+	next_sample_ = 0.f;
+    phase_ += frequency_;
+	if(phase_ >= 1.0f)
 	{
-        phase -= 1.0f;
-        float t = phase / frequency_;
+        phase_ -= 1.0f;
+        float t = phase_ / frequency_;
         // t = 0: the transition occurred right at this sample.
         // t = 1: the transition occurred at the previous sample.
         // Use linear interpolation to recover the fractional sample.
         float new_sample
-            = previous_sample + (in - previous_sample) * (1.0f - t);
-        float discontinuity = new_sample - sample;
+            = previous_sample_ + (in - previous_sample_) * (1.0f - t);
+        float discontinuity = new_sample - sample_;
         this_sample += discontinuity * ThisBlepSample(t);
-        next_sample += discontinuity * NextBlepSample(t);
-        sample = new_sample;
+        next_sample_ = discontinuity * NextBlepSample(t);
+        sample_ = new_sample;
     }
-    next_sample += sample;
-    previous_sample = in;
-
-    phase_           = phase;
-    next_sample_     = next_sample;
-    sample_          = sample;
-    previous_sample_ = previous_sample;
+    next_sample_ += sample_;
+    previous_sample_ = in;
 
     return this_sample;
 }
