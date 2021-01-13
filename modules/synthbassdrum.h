d955780bb06b0e7e7d6574a5a5fb61db9e524bdb
U-MSI\Ben <bensergentanis@gmail.com>
2021-01-13 14:10:32 -0500

    formatting

--- modules/synthbassdrum.h
+++ modules/synthbassdrum.h
@@ -146,7 +146,7 @@ class SyntheticBassDrum
   private:
     float sample_rate_;
 
-	bool sustain_;
+    bool  sustain_;
     float accent_, new_f0_, tone_, decay_;
     float dirtiness_, fm_envelope_amount_, fm_envelope_decay_;
 
