8cbba0b753566d9c6748ea69d1c85647cdcd4cfd
U-MSI\Ben <bensergentanis@gmail.com>
2021-01-14 15:36:27 -0500

    more setup

---
+++ modules/diffuser.cpp
@@ -0,0 +1,67 @@
+#include "dsp.h"
+#include "diffuser.h"
+#include <math.h>
+
+using namespace daisysp;
+
+void Diffuser::Init(uint16_t* buffer)
+{
+    engine_.Init(buffer);
+    engine_.SetLFOFrequency(LFO_1, 0.3f / 48000.0f);
+    lp_decay_ = 0.0f;
+}
+
+void Diffuser::Clear()
+{
+    engine_.Clear();
+}
+
+void Diffuser::Process(float amount, float rt, float* in_out, size_t size)
+{
+    typedef E::Reserve<
+        126,
+        E::Reserve<
+            180,
+            E::Reserve<
+                269,
+                E::Reserve<
+                    444,
+                    E::Reserve<1653, E::Reserve<2010, E::Reserve<3411>>>>>>>
+                            Memory;
+    E::DelayLine<Memory, 0> ap1;
+    E::DelayLine<Memory, 1> ap2;
+    E::DelayLine<Memory, 2> ap3;
+    E::DelayLine<Memory, 3> ap4;
+    E::DelayLine<Memory, 4> dapa;
+    E::DelayLine<Memory, 5> dapb;
+    E::DelayLine<Memory, 6> del;
+    E::Context              c;
+    const float             kap = 0.625f;
+    const float             klp = 0.75f;
+    float                   lp  = lp_decay_;
+    while(size--)
+    {
+        float wet;
+        engine_.Start(&c);
+        c.Read(*in_out);
+        c.Read(ap1 TAIL, kap);
+        c.WriteAllPass(ap1, -kap);
+        c.Read(ap2 TAIL, kap);
+        c.WriteAllPass(ap2, -kap);
+        c.Read(ap3 TAIL, kap);
+        c.WriteAllPass(ap3, -kap);
+        c.Interpolate(ap4, 400.0f, LFO_1, 43.0f, kap);
+        c.WriteAllPass(ap4, -kap);
+        c.Interpolate(del, 3070.0f, LFO_1, 340.0f, rt);
+        c.Lp(lp, klp);
+        c.Read(dapa TAIL, -kap);
+        c.WriteAllPass(dapa, kap);
+        c.Read(dapb TAIL, kap);
+        c.WriteAllPass(dapb, -kap);
+        c.Write(del, 2.0f);
+        c.Write(wet, 0.0f);
+        *in_out += amount * (wet - *in_out);
+        ++in_out;
+    }
+    lp_decay_ = lp;
+}
---
+++ modules/diffuser.h
@@ -0,0 +1,39 @@
+#pragma once
+#ifndef DSY_DIFFUSER_H
+#define DSY_DIFFUSER_H
+
+#include <stdint.h>
+#ifdef __cplusplus
+
+/** @file diffuser.h */
+
+namespace daisysp
+{
+/**  
+       @brief Granular Diffuser
+	   @author Ben Sergentanis
+	   @date Jan 2021
+	   Ported from pichenettes/eurorack/plaits/dsp/fx/diffuser.h \n
+	   to an independent module. \n
+	   Original code written by Emilie Gillet in 2016. \n
+*/
+class Diffuser
+{
+  public:
+    Diffuser() {}
+    ~Diffuser() {}
+
+    void Init(uint16_t* buffer);
+
+    void Clear() { engine_.Clear(); }
+
+    void Process(float amount, float rt, float* in_out, size_t size);
+	
+  private:
+    typedef FxEngine<8192, FORMAT_12_BIT> E;
+    E                                     engine_;
+    float                                 lp_decay_;
+};
+} // namespace daisysp
+#endif
+#endif
