af27c4e8b88efaaff8bcc3f890f34e47ae94d209
U-MSI\Ben <bensergentanis@gmail.com>
2021-01-15 09:53:04 -0500

    recalc moved to setdrive

--- modules/overdrive.cpp
+++ modules/overdrive.cpp
@@ -6,30 +6,27 @@ using namespace daisysp;
 
 void Overdrive::Init()
 {
-    drive_     = 0.f;
-    pre_gain_  = 0.0f;
-    post_gain_ = 0.0f;
+    SetDrive(.5f);
 }
 
 float Overdrive::Process(float in)
 {
-    const float drive_2    = drive_ * drive_;
-    const float pre_gain_a = drive_ * 0.5f;
-    const float pre_gain_b = drive_2 * drive_2 * drive_ * 24.0f;
-    const float pre_gain_  = pre_gain_a + (pre_gain_b - pre_gain_a) * drive_2;
-    const float drive_squashed = drive_ * (2.0f - drive_);
-    const float post_gain_
-        = 1.0f / SoftClip(0.33f + drive_squashed * (pre_gain_ - 0.33f));
-
     float pre = pre_gain_ * in;
     return SoftClip(pre) * post_gain_;
 }
 
-
 void Overdrive::SetDrive(float drive)
 {
-    drive  = DSY_CLAMP(drive, 0, 1);
+    drive  = fclamp(drive, 0.f, 1.f);
     drive_ = 2.f * drive;
+
+    const float drive_2    = drive_ * drive_;
+    const float pre_gain_a = drive_ * 0.5f;
+    const float pre_gain_b = drive_2 * drive_2 * drive_ * 24.0f;
+    pre_gain_              = pre_gain_a + (pre_gain_b - pre_gain_a) * drive_2;
+
+    const float drive_squashed = drive_ * (2.0f - drive_);
+    post_gain_ = 1.0f / SoftClip(0.33f + drive_squashed * (pre_gain_ - 0.33f));
 }
 
 inline float Overdrive::SoftLimit(float x)
