d5e91a8346debe24504bfd20298356ab8cb35408
U-MSI\Ben <bensergentanis@gmail.com>
2021-01-18 10:47:17 -0500

    building

--- modules/modalvoice.h
+++ modules/modalvoice.h
@@ -1,68 +1,53 @@
-// Copyright 2016 Emilie Gillet.
-//
-// Author: Emilie Gillet (emilie.o.gillet@gmail.com)
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-// 
-// See http://creativecommons.org/licenses/MIT/ for more information.
-//
-// -----------------------------------------------------------------------------
-//
-// Simple modal synthesis voice with a mallet exciter:
-// click -> LPF -> resonator.
-// 
-// The click is replaced by continuous white noise when the trigger input
-// of the module is not patched.
+#pragma once
+#ifndef DSY_MODAL_H
+#define DSY_MODAL_H
 
-#ifndef PLAITS_DSP_PHYSICAL_MODELLING_MODAL_VOICE_H_
-#define PLAITS_DSP_PHYSICAL_MODELLING_MODAL_VOICE_H_
+#include <stdint.h>
+#include "modules/svf.h"
+#include "modules/resonator.h"
+#include "modules/dust.h"
+#ifdef __cplusplus
 
-#include "plaits/dsp/physical_modelling/resonator.h"
+/** @file modalvoice.h */
 
-namespace plaits {
+namespace daisysp
+{
+/**  
+       @brief Simple modal synthesis voice with a mallet exciter: click -> LPF -> resonator.
+	   @author Ben Sergentanis
+	   @date Jan 2021
+	   The click can be replaced by continuous white noise. \n \n 
+	   Ported from pichenettes/eurorack/plaits/dsp/physical_modelling/modal_voice.h \n
+	   and pichenettes/eurorack/plaits/dsp/physical_modelling/modal_voice.cc \n 
+	   to an independent module. \n
+	   Original code written by Emilie Gillet in 2016. \n
+*/
+class ModalVoice
+{
+  public:
+    ModalVoice() {}
+    ~ModalVoice() {}
 
-class ModalVoice {
- public:
-  ModalVoice() { }
-  ~ModalVoice() { }
-  
-  void Init();
-  void Render(
-      bool sustain,
-      bool trigger,
-      float accent,
-      float f0,
-      float structure,
-      float brightness,
-      float damping,
-      float* temp,
-      float* out,
-      float* aux,
-      size_t size);
-  
- private:
-  ResonatorSvf<1> excitation_filter_;
-  Resonator resonator_;
-  
-  DISALLOW_COPY_AND_ASSIGN(ModalVoice);
+    void Init(float sample_rate);
+    void Process(bool   sustain,
+                 bool   trigger,
+                 float  accent,
+                 float  f0,
+                 float  structure,
+                 float  brightness,
+                 float  damping,
+                 float* temp,
+                 float* out,
+                 float* aux,
+                 size_t size);
+
+  private:
+    float sample_rate_;
+
+    ResonatorSvf<1> excitation_filter_;
+    Resonator       resonator_;
+    Dust            dust_;
 };
-
-}  // namespace plaits
-
-#endif  // PLAITS_DSP_PHYSICAL_MODELLING_MODAL_VOICE_H_
+} // namespace daisysp
+#endif
+#endif
