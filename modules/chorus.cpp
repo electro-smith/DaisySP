305948bf9309eb35e380925d6329e8dab4c449b9
U-MSI\Ben <bensergentanis@gmail.com>
2021-01-20 13:39:57 -0500

    initial try

--- modules/chorus.cpp
+++ modules/chorus.cpp
@@ -7,8 +7,44 @@ using namespace daisysp;
 void Chorus::Init(float sample_rate){
 	sample_rate_ = sample_rate;
 	
+	max_del_ = .06f * sample_rate_; //60 ms worth of samples
+	pre_gain_ = 1.f / NUM_DEL;
+	
+	const float inc = .1f / sample_rate_;
+	for(int i = 0; i < NUM_DEL; i++){
+		del_[i].Init();
+		
+		lfo_phase_[i] = i * pre_gain_; //start out of phase
+		lfo_freq_[i] = inc + inc * i; //different freqs
+		lfo_amp_[i] = 1.f;
+	}
+}
+
+float Chorus::ProcessLfo(int idx){
+	lfo_phase_[idx] += lfo_freq_[idx];
+
+	if(lfo_phase_[idx] >= 1.f){
+		lfo_phase_[idx] = 1.f - (lfo_phase_[idx] - 1.f);
+		lfo_freq_[idx] *= -1;
+	}
+	else if(lfo_phase_[idx] <= 1.f){
+		lfo_phase_[idx] = -1.f * lfo_phase_[idx];
+		lfo_freq_[idx] *= -1;
+	}
+	
+	return lfo_phase_[idx] * lfo_amp_[idx];
 }
 
 float Chorus::Process(float in){
-	return 0.f;
+	float ret = 0.f;
+
+	for(int i = 0; i < NUM_DEL; i++){
+		float lfo_sig = ProcessLfo(i);
+		del_[i].SetDelay(lfo_sig * max_del_);
+		del_[i].Write(in);
+		
+		ret += del_[i].Read();
+	}
+	
+	return ret * pre_gain_;
 }
--- modules/chorus.h
+++ modules/chorus.h
@@ -1,9 +1,13 @@
 #pragma once
 #ifndef DSY_CHORUS_H
 #define DSY_CHORUS_H
+#ifdef __cplusplus
 
 #include <stdint.h>
-#ifdef __cplusplus
+#include "modules/delayline.h"
+
+#define NUM_DEL 1
+#define DEL_LEN 48000 
 
 /** @file chorus.h */
 
@@ -14,6 +18,7 @@ namespace daisysp
 	@author Ben Sergentanis
 	@date Jan 2021
 	Based on https://www.izotope.com/en/learn/understanding-chorus-flangers-and-phasers-in-audio-production.html \n
+	and https://www.researchgate.net/publication/236629475_Implementing_Professional_Audio_Effects_with_DSPs \n
 */
 class Chorus
 {
@@ -25,8 +30,22 @@ class Chorus
 	
 	float Process(float in);
 	
+	void SetDryWet(float wet);
+	
   private:
 	float sample_rate_;
+
+	//triangle lfos
+	float ProcessLfo(int idx);
+	
+	float lfo_phase_[NUM_DEL];
+	float lfo_freq_[NUM_DEL];
+	float lfo_amp_[NUM_DEL];
+
+	float max_del_;
+	float pre_gain_;
+		
+	static DelayLine<float, DEL_LEN> del_[NUM_DEL];
 };
 } //namespace daisysp
 #endif
