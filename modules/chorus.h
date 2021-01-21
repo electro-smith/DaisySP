fddc0e37b944552a3890756bf3407519912ee076
U-MSI\Ben <bensergentanis@gmail.com>
2021-01-21 10:08:50 -0500

    setters

--- modules/chorus.h
+++ modules/chorus.h
@@ -7,7 +7,7 @@
 #include "modules/delayline.h"
 
 #define NUM_DEL 2
-#define DEL_LEN 48000 
+#define DEL_LEN 48000
 
 /** @file chorus.h */
 
@@ -25,58 +25,42 @@ class Chorus
   public:
     Chorus() {}
     ~Chorus() {}
-	
-	void Init(float sample_rate);
-	
-	float Process(float in);
-	
-	/** How much +/- the lfo will move its delayline in ms. */
-	void SetLfoDepth(float depth, int idx = 0){
-		if(idx < 0 || idx >= NUM_DEL){
-			return;
-		}
-		
-		depth *= sample_rate_ * .001f;
-		lfo_amp_[idx] = fclamp(depth, 0.f, delay_[idx]);
-	}
-	
-	/** How fast the lfo will cycle in Hz */
-	void SetLfoFreq(float freq, int idx = 0){
-		if(idx < 0 || idx >= NUM_DEL){
-			return;
-		}
-		
-		freq = 4.f * freq / sample_rate_;
-		freq *= lfo_freq_[idx] < 0.f ? -1.f : 1.f; //if we're headed down, keep going
-		lfo_freq_[idx] = fclamp(freq, -.25f, .25f); //clip at +/- .125 * sr
-	}
-	
-	/** This delayline's base delay in ms */
-	void SetDelay(float ms, int idx = 0){
-		if(idx < 0 || idx >= NUM_DEL){
-			return;
-		}
-		
-		ms *= .001f * sample_rate_;
-		delay_[idx] = fmax(ms, 0.0001f * sample_rate_);
-		
-		lfo_amp_[idx] = fmin(lfo_amp_[idx], delay_[idx]); //clip this if needed
-	}
-	
+
+    void Init(float sample_rate);
+
+    float Process(float in);
+
+    /** How much +/- the lfo will move its delayline in ms. */
+    void SetLfoDepth(float depth, int idx = 0);
+
+    /** How fast the lfo will cycle in Hz */
+    void SetLfoFreq(float freq, int idx = 0);
+
+    /** This delayline's base delay in ms */
+    void SetDelay(float ms, int idx = 0);
+
+    void SetLfoDepthAll(float depth);
+
+    /** How fast the lfo will cycle in Hz */
+    void SetLfoFreqAll(float freq);
+
+    /** This delayline's base delay in ms */
+    void SetDelayAll(float ms);
+
   private:
-	float sample_rate_;
+    float sample_rate_;
 
-	//triangle lfos
-	float ProcessLfo(int idx);
-	
-	float lfo_phase_[NUM_DEL];
-	float lfo_freq_[NUM_DEL];
-	float lfo_amp_[NUM_DEL];
+    //triangle lfos
+    float ProcessLfo(int idx);
 
-	float delay_[NUM_DEL];
-	float pre_gain_;
-		
-	DelayLine<float, DEL_LEN> del_[NUM_DEL];
+    float lfo_phase_[NUM_DEL];
+    float lfo_freq_[NUM_DEL];
+    float lfo_amp_[NUM_DEL];
+
+    float delay_[NUM_DEL];
+    float pre_gain_;
+
+    DelayLine<float, DEL_LEN> del_[NUM_DEL];
 };
 } //namespace daisysp
 #endif
