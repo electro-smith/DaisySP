0063cd987b858d23b927c06e25aa5cb737ae7321
U-MSI\Ben <bensergentanis@gmail.com>
2021-01-28 19:37:58 -0500

    better implementation

--- modules/chorus.h
+++ modules/chorus.h
@@ -10,139 +10,64 @@
 
 namespace daisysp
 {
-//does the hevy lifting
-template <int NUM_DEL>
+//does the heavy lifting
 class ChorusEngine
 {
-  public:
-    void Init(float sample_rate)
-    {
-        sample_rate_ = sample_rate;
+	public:
+	ChorusEngine() {}
+	~ChorusEngine() {}
+    
+	/** Initialize the module
+		\param sample_rate Audio engine sample rate.
+	*/
+    void Init(float sample_rate);
 
-        pre_gain_ = 1.f / (NUM_DEL + 1);
+    /** Get the next sample
+		\param in Sample to process
+	*/
+    float Process(float in);
 
-        for(int i = 0; i < NUM_DEL; i++)
-        {
-            del_[i].Init();
-            lfo_amp_[i] = 0.f;
-            SetDelay(2 + 2 * i, i);
+    /** How much of the signal to feedback into the delay line.
+		\param feedback Works 0-1.
+	*/
+    void SetFeedback(float feedback);
 
-            lfo_phase_[i] = 0.f;
-            SetLfoFreq(.5 + .05 * i, i);
-            SetLfoDepth(2, i);
-        }
-    }
+    /** How much to modulate the delay by.
+		\param depth Works 0-1.
+	*/
+    void SetLfoDepth(float depth);
 
-    float Process(float in)
-    {
-        float ret = 0.f;
+    /** Set lfo frequency.
+		\param freq Frequency in Hz
+	*/
+    void SetLfoFreq(float freq);
 
-        for(int i = 0; i < NUM_DEL; i++)
-        {
-            float lfo_sig = ProcessLfo(i);
-            del_[i].SetDelay(lfo_sig + delay_[i]);
+    /** Set the internal delay rate. 
+		\param delay Tuned for 0-1. Maps to .1 to 50 ms.
+	*/
+    void SetDelay(float delay);
 
-            ret += del_[i].Read();
-            del_[i].Write(in);
-        }
-
-        return in * pre_gain_ + ret * pre_gain_;
-    }
-
-    void SetLfoDepth(float depth, int idx = 0)
-    {
-        if(idx < 0 || idx >= NUM_DEL)
-        {
-            return;
-        }
-
-        depth *= sample_rate_ * .001f;
-        lfo_amp_[idx] = fclamp(depth, 0.f, delay_[idx]);
-    }
-
-    void SetLfoFreq(float freq, int idx = 0)
-    {
-        if(idx < 0 || idx >= NUM_DEL)
-        {
-            return;
-        }
-
-        freq = 4.f * freq / sample_rate_;
-        freq *= lfo_freq_[idx] < 0.f ? -1.f
-                                     : 1.f; //if we're headed down, keep going
-        lfo_freq_[idx] = fclamp(freq, -.25f, .25f); //clip at +/- .125 * sr
-    }
-
-    void SetDelay(float ms, int idx = 0)
-    {
-        if(idx < 0 || idx >= NUM_DEL)
-        {
-            return;
-        }
-
-        ms *= .001f * sample_rate_;
-        delay_[idx] = fmax(ms, 0.0001f * sample_rate_);
-
-        lfo_amp_[idx] = fmin(lfo_amp_[idx], delay_[idx]); //clip this if needed
-    }
-
-    void SetLfoDepthAll(float depth)
-    {
-        for(int i = 0; i < NUM_DEL; i++)
-        {
-            SetLfoDepth(depth, i);
-        }
-    }
-
-    void SetLfoFreqAll(float freq)
-    {
-        for(int i = 0; i < NUM_DEL; i++)
-        {
-            SetLfoFreq(freq, i);
-        }
-    }
-
-    void SetDelayAll(float ms)
-    {
-        for(int i = 0; i < NUM_DEL; i++)
-        {
-            SetDelay(ms, i);
-        }
-    }
+    /** Set the delay time in ms.
+		\param ms Delay time in ms.
+	*/
+    void SetDelayMs(float ms);
 
   private:
     float                    sample_rate_;
-    static constexpr int32_t kDelayLength
-        = 2400; // 50 ms at 48kHz = .05 * 48000
+    static constexpr int32_t kDelayLength = 2400; // 50 ms at 48kHz = .05 * 48000
+
+    float feedback_;
 
     //triangle lfos
-    float lfo_phase_[NUM_DEL];
-    float lfo_freq_[NUM_DEL];
-    float lfo_amp_[NUM_DEL];
+    float lfo_phase_;
+    float lfo_freq_;
+    float lfo_amp_;
 
-    float delay_[NUM_DEL];
-    float pre_gain_;
+    float delay_;
 
-    DelayLine<float, kDelayLength> del_[NUM_DEL];
+    DelayLine<float, kDelayLength> del_;
 
-    float ProcessLfo(int idx)
-    {
-        lfo_phase_[idx] += lfo_freq_[idx];
-
-        //wrap around and flip direction
-        if(lfo_phase_[idx] > 1.f)
-        {
-            lfo_phase_[idx] = 1.f - (lfo_phase_[idx] - 1.f);
-            lfo_freq_[idx] *= -1.f;
-        }
-        else if(lfo_phase_[idx] < -1.f)
-        {
-            lfo_phase_[idx] = -1.f - (lfo_phase_[idx] + 1.f);
-            lfo_freq_[idx] *= -1.f;
-        }
-
-        return lfo_phase_[idx] * lfo_amp_[idx];
-    }
+    float ProcessLfo();
 };
 
 //wraps up all of the chorus engines
@@ -153,7 +78,7 @@ class ChorusEngine
 	Based on https://www.izotope.com/en/learn/understanding-chorus-flangers-and-phasers-in-audio-production.html \n
 	and https://www.researchgate.net/publication/236629475_Implementing_Professional_Audio_Effects_with_DSPs \n
 */
-template <int NUM_DEL = 2, int NUM_CHANNELS = 2>
+template <int NUM_DELS = 2>
 class Chorus
 {
   public:
@@ -162,58 +87,86 @@ class Chorus
 
     void Init(float sample_rate)
     {
-        for(int i = 0; i < NUM_CHANNELS; i++)
+		float f = .5f;
+		if(NUM_DELS > 1){
+			f = 1.f / (float)(NUM_DELS - 1);
+        }
+		
+		for(int i = 0; i < NUM_DELS; i++)
         {
             engines_[i].Init(sample_rate);
+			SetPan(i * f, i);
         }
+				
+		gain_frac_ = 1.f / (float)NUM_DELS;
     }
 
-    float Process(float in, int channel = 0)
+	void Process(float in, float& outl, float& outr){
+		float l = 0.f;
+		float r = 0.f;
+
+		for(int i = 0; i < NUM_DELS; i++){
+			float sig = engines_[i].Process(in);
+			l += (1.f - pan_[i]) * sig;
+			r += pan_[i] * sig;
+		}
+		
+		outl = l * gain_frac_;
+		outr = r * gain_frac_;
+	}
+
+    float ProcessSingle(float in, int delnum = 0)
     {
-        return engines_[channel].Process(in);
+        return engines_[delnum].Process(in);
+    }
+	
+	void SetPan(float pan, int delnum = 0){
+		pan_[delnum] = fclamp(pan, 0.f, 1.f);;
+	}
+
+    void SetLfoDepth(float depth, int delnum = 0)
+    {
+        engines_[delnum].SetLfoDepth(depth);
     }
 
-    void SetLfoDepth(float depth, int idx = 0, int channel = 0)
+    void SetLfoFreq(float freq, int delnum = 0)
     {
-        engines_[channel].SetLfoDepth(depth, idx);
+        engines_[delnum].SetLfoFreq(freq);
     }
 
-    void SetLfoFreq(float freq, int idx = 0, int channel = 0)
+    void SetDelay(float ms, int delnum = 0)
     {
-        engines_[channel].SetLfoFreq(freq, idx);
-    }
-
-    void SetDelay(float ms, int idx = 0, int channel = 0)
-    {
-        engines_[channel].SetDelay(ms, idx);
+        engines_[delnum].SetDelay(ms);
     }
 
     void SetLfoDepthAll(float depth)
     {
-        for(int i = 0; i < NUM_CHANNELS; i++)
+        for(int i = 0; i < NUM_DELS; i++)
         {
-            engines_[i].SetLfoDepthAll(depth);
+            engines_[i].SetLfoDepth(depth);
         }
     }
 
     void SetLfoFreqAll(float freq)
     {
-        for(int i = 0; i < NUM_CHANNELS; i++)
+        for(int i = 0; i < NUM_DELS; i++)
         {
-            engines_[i].SetLfoFreqAll(freq);
+            engines_[i].SetLfoFreq(freq);
         }
     }
 
     void SetDelayAll(float ms)
     {
-        for(int i = 0; i < NUM_CHANNELS; i++)
+        for(int i = 0; i < NUM_DELS; i++)
         {
-            engines_[i].SetDelayAll(ms);
+            engines_[i].SetDelay(ms);
         }
     }
 
   private:
-    ChorusEngine<NUM_DEL> engines_[NUM_CHANNELS];
+    ChorusEngine engines_[NUM_DELS];
+	float gain_frac_;
+	float pan_[NUM_DELS];
 };
 } //namespace daisysp
 #endif
