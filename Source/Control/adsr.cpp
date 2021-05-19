b8add11b3816da8ce69e48c2ccd3524d0d0cc5d2
sdiedrichsen <47812706+sdiedrichsen@users.noreply.github.com>
2021-05-19 13:26:05 +0200

    Conform to clang format

--- Source/Control/adsr.cpp
+++ Source/Control/adsr.cpp
@@ -7,22 +7,21 @@ using namespace daisysp;
 void Adsr::Init(float sample_rate, int blockSize)
 {
     sample_rate_ = sample_rate / blockSize;
-    SetTime(ADSR_SEG_ATTACK,  0.1f);
-    SetTime(ADSR_SEG_DECAY,   0.1f);
+    SetTime(ADSR_SEG_ATTACK, 0.1f);
+    SetTime(ADSR_SEG_DECAY, 0.1f);
     SetTime(ADSR_SEG_RELEASE, 0.1f);
-    sus_level_  = 0.7f;
-    x_          = 0.0f;
+    sus_level_ = 0.7f;
+    x_         = 0.0f;
 }
 
 void Adsr::Retrigger(bool hard)
 {
     mode_ = ADSR_SEG_ATTACK;
     if(hard)
-        x_ = 0.f;
+      x_ = 0.f;
 }
 
-void
-Adsr::SetTime(int seg, float time)
+void Adsr::SetTime(int seg, float time)
 {
     if(seg_time_[seg] != time)
     {
@@ -30,12 +29,13 @@ Adsr::SetTime(int seg, float time)
         if(time > 0.f)
         {
             const float attackTarget = logf(0.5f);
-            const float decayTarget  = logf(1./M_E);
-            float target = (seg == ADSR_SEG_ATTACK)? attackTarget : decayTarget;
-            seg_D0_  [seg] = 1.f - expf(target / (time * sample_rate_));
+            const float decayTarget  = logf(1. / M_E);
+            float       target
+                = (seg == ADSR_SEG_ATTACK) ? attackTarget : decayTarget;
+            seg_D0_[seg] = 1.f - expf(target / (time * sample_rate_));            
         }
         else
-            seg_D0_  [seg] = 1.f; // instant change
+            seg_D0_[seg] = 1.f; // instant change
     }
 }
 
@@ -50,7 +50,7 @@ float Adsr::Process(bool gate)
         mode_ = ADSR_SEG_RELEASE;
 
     float D0(seg_D0_[mode_]);
-    float target = mode_ == ADSR_SEG_DECAY? sus_level_ : - 0.1f;
+    float target = mode_ == ADSR_SEG_DECAY ? sus_level_ : - 0.1f;
     switch(mode_)
     {
         case ADSR_SEG_IDLE: out = 0.0f; break;
@@ -59,9 +59,8 @@ float Adsr::Process(bool gate)
             out = x_;
             if(out > 1.f)
             {
-                x_  =
-                out = 1.f;
-                mode_ = ADSR_SEG_DECAY;
+                x_  = out = 1.f;
+                mode_     = ADSR_SEG_DECAY;
             }
             break;
         case ADSR_SEG_DECAY:
@@ -70,9 +69,8 @@ float Adsr::Process(bool gate)
             out = x_;
             if(out < 0.0f)
             {
-                x_  =
-                out = 0.f;
-                mode_ = ADSR_SEG_IDLE;
+                x_ = out = 0.f;
+                mode_    = ADSR_SEG_IDLE;
             }
         default: break;
     }
