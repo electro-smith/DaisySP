6f8d2e7e08e6ae6795f03bd750f50d67d6fc683e
sdiedrichsen <47812706+sdiedrichsen@users.noreply.github.com>
2021-06-05 01:41:36 +0200

    Adsr remake (#160)
    * Add block size to Init() for block based processing
    
    * Remove unused variables
    
    * Add improved SetTime method
    
    It's based on a geometric series
    
    * Redo processing with 1st order SVF
    
    * remove unused methods
    
    * Clamp sustain level
    
    * Fix Init()
    
    * Add method for retrigger
    
    * Avoid div by zero
    
    * Some formatting
    
    * Optimize coefficient calculation
    
    * Fix unwanted repetition
    
    with all time set to 0. and sustain at 0.
    
    * Fix warning about missing inline function
    
    * Sign my work
    
    * Conform to clang format
    
    * Conform to clang format
    
    * Fix a typo
    
    * Conform to flag format pt. 2.
    
    * Conform to clang format pt.3
    
    * Clang Format pt.4
    
    * Checked with local clang, no issues
    
    * Conform to clang format pt.5
    
    I used clang format v12 on my system, but v10 seems to have a different idea about nested trigraphs.
    
    * Remove very white space
    
    * Attack with shape added, some refinements
    
    * Conform to clang-format
    
    v12 has a different opinion on that one.

--- Source/Control/adsr.cpp
+++ Source/Control/adsr.cpp
@@ -3,79 +3,133 @@
 
 using namespace daisysp;
 
-float Adsr::Tau2Pole(float tau)
+
+void Adsr::Init(float sample_rate, int blockSize)
 {
-    return expf(-1.0f / (tau * sample_rate_));
+    sample_rate_  = sample_rate / blockSize;
+    attackShape_  = -1.f;
+    attackTarget_ = 0.0f;
+    attackTime_   = -1.f;
+    decayTime_    = -1.f;
+    releaseTime_  = -1.f;
+    sus_level_    = 0.7f;
+    x_            = 0.0f;
+    gate_         = false;
+    mode_         = ADSR_SEG_IDLE;
+
+    SetTime(ADSR_SEG_ATTACK, 0.1f);
+    SetTime(ADSR_SEG_DECAY, 0.1f);
+    SetTime(ADSR_SEG_RELEASE, 0.1f);
 }
 
-float Adsr::AdsrFilter()
+void Adsr::Retrigger(bool hard)
 {
-    y_ = b_ * x_ + a_ * y_;
-    return y_;
+    mode_ = ADSR_SEG_ATTACK;
+    if(hard)
+        x_ = 0.f;
+}
+
+void Adsr::SetTime(int seg, float time)
+{
+    switch(seg)
+    {
+        case ADSR_SEG_ATTACK: SetAttackTime(time, 0.0f); break;
+        case ADSR_SEG_DECAY:
+        {
+            SetTimeConstant(time, decayTime_, decayD0_);
+        }
+        break;
+        case ADSR_SEG_RELEASE:
+        {
+            SetTimeConstant(time, releaseTime_, releaseD0_);
+        }
+        break;
+        default: return;
+    }
+}
+
+void Adsr::SetAttackTime(float timeInS, float shape)
+{
+    if((timeInS != attackTime_) || (shape != attackShape_))
+    {
+        attackTime_  = timeInS;
+        attackShape_ = shape;
+        if(timeInS > 0.f)
+        {
+            float x         = shape;
+            float target    = 9.f * powf(x, 10.f) + 0.3f * x + 1.01f;
+            attackTarget_   = target;
+            float logTarget = logf(1.f - (1.f / target)); // -1 for decay
+            attackD0_       = 1.f - expf(logTarget / (timeInS * sample_rate_));
+        }
+        else
+            attackD0_ = 1.f; // instant change
+    }
+}
+void Adsr::SetDecayTime(float timeInS)
+{
+    SetTimeConstant(timeInS, decayTime_, decayD0_);
+}
+void Adsr::SetReleaseTime(float timeInS)
+{
+    SetTimeConstant(timeInS, releaseTime_, releaseD0_);
 }
 
 
-void Adsr::Init(float sample_rate)
+void Adsr::SetTimeConstant(float timeInS, float& time, float& coeff)
 {
-    seg_time_[ADSR_SEG_ATTACK]  = 0.1f;
-    seg_time_[ADSR_SEG_DECAY]   = 0.1f;
-    sus_                        = 0.7f;
-    seg_time_[ADSR_SEG_RELEASE] = 0.1f;
-    //timer_ = 0;
-    a_           = 0.0f;
-    b_           = 0.0f;
-    x_           = 0.0f;
-    y_           = 0.0f;
-    prev_        = 0.0f;
-    atk_time_    = seg_time_[ADSR_SEG_ATTACK] * sample_rate;
-    sample_rate_ = sample_rate;
+    if(timeInS != time)
+    {
+        time = timeInS;
+        if(time > 0.f)
+        {
+            const float target = logf(1. / M_E);
+            coeff              = 1.f - expf(target / (time * sample_rate_));
+        }
+        else
+            coeff = 1.f; // instant change
+    }
 }
+
 
 float Adsr::Process(bool gate)
 {
-    float pole, out;
-    out = 0.0f;
+    float out = 0.0f;
 
-    if(gate && mode_ != ADSR_SEG_DECAY)
-    {
+    if(gate && !gate_) // rising edge
         mode_ = ADSR_SEG_ATTACK;
-        //timer_ = 0;
-        pole      = Tau2Pole(seg_time_[ADSR_SEG_ATTACK] * 0.6f);
-        atk_time_ = seg_time_[ADSR_SEG_ATTACK] * sample_rate_;
-        a_        = pole;
-        b_        = 1.0f - pole;
-    }
-    else if(!gate && mode_ != ADSR_SEG_IDLE)
-    {
+    else if(!gate && gate_) // falling edge
         mode_ = ADSR_SEG_RELEASE;
-        pole  = Tau2Pole(seg_time_[ADSR_SEG_RELEASE]);
-        a_    = pole;
-        b_    = 1.0f - pole;
-    }
+    gate_ = gate;
 
-    x_    = (int)gate;
-    prev_ = (int)gate;
+    float D0(attackD0_);
+    if(mode_ == ADSR_SEG_DECAY)
+        D0 = decayD0_;
+    else if(mode_ == ADSR_SEG_RELEASE)
+        D0 = releaseD0_;
 
+    float target = mode_ == ADSR_SEG_DECAY ? sus_level_ : -0.01f;
     switch(mode_)
     {
         case ADSR_SEG_IDLE: out = 0.0f; break;
         case ADSR_SEG_ATTACK:
-            out = AdsrFilter();
-
-            if(out > .99f)
+            x_ += D0 * (attackTarget_ - x_);
+            out = x_;
+            if(out > 1.f)
             {
-                mode_ = ADSR_SEG_DECAY;
-                pole  = Tau2Pole(seg_time_[ADSR_SEG_DECAY]);
-                a_    = pole;
-                b_    = 1.0f - pole;
+                x_ = out = 1.f;
+                mode_    = ADSR_SEG_DECAY;
             }
             break;
         case ADSR_SEG_DECAY:
         case ADSR_SEG_RELEASE:
-            x_ *= sus_;
-            out = AdsrFilter();
-            if(out <= 0.01f)
-                mode_ = ADSR_SEG_IDLE;
+            x_ += D0 * (target - x_);
+            out = x_;
+            if(out < 0.0f)
+            {
+                x_ = out = 0.f;
+                mode_    = ADSR_SEG_IDLE;
+            }
         default: break;
     }
     return out;
--- Source/Control/adsr.h
+++ Source/Control/adsr.h
@@ -11,18 +11,14 @@ namespace daisysp
 - IDLE   = located at phase location 0, and not currently running
 - ATTACK  = First segment of envelope where phase moves from 0 to 1
 - DECAY   = Second segment of envelope where phase moves from 1 to SUSTAIN value
-- SUSTAIN = Third segment of envelope, stays at SUSTAIN level until GATE is released
 - RELEASE =     Fourth segment of envelop where phase moves from SUSTAIN to 0
-- LAST    =  Last segment, aka release
 */
 enum
 {
-    ADSR_SEG_IDLE,
-    ADSR_SEG_ATTACK,
-    ADSR_SEG_DECAY,
-    ADSR_SEG_SUSTAIN,
-    ADSR_SEG_RELEASE,
-    ADSR_SEG_LAST,
+    ADSR_SEG_IDLE    = 0,
+    ADSR_SEG_ATTACK  = 1,
+    ADSR_SEG_DECAY   = 2,
+    ADSR_SEG_RELEASE = 4
 };
 
 
@@ -31,6 +27,8 @@ enum
 Original author(s) : Paul Batchelor
 
 Ported from Soundpipe by Ben Sergentanis, May 2020
+ 
+Remake by Steffan DIedrichsen, May 2021
 */
 class Adsr
 {
@@ -40,22 +38,37 @@ class Adsr
     /** Initializes the Adsr module.
         \param sample_rate - The sample rate of the audio engine being run. 
     */
-    void Init(float sample_rate);
-
-
+    void Init(float sample_rate, int blockSize = 1);
+    /**
+     \function Retrigger forces the envelope back to attack phase
+     \param hard  resets the history to zero, results in a click.
+     */
+    void Retrigger(bool hard);
     /** Processes one sample through the filter and returns one sample.
         \param gate - trigger the envelope, hold it to sustain 
     */
     float Process(bool gate);
-
     /** Sets time
         Set time per segment in seconds
     */
-    inline void SetTime(int seg, float time) { seg_time_[seg] = time; }
+    void SetTime(int seg, float time);
+    void SetAttackTime(float timeInS, float shape = 0.0f);
+    void SetDecayTime(float timeInS);
+    void SetReleaseTime(float timeInS);
+
+  private:
+    void SetTimeConstant(float timeInS, float& time, float& coeff);
+
+  public:
     /** Sustain level
-        \param sus_level - sets sustain level
+        \param sus_level - sets sustain level, 0...1.0
     */
-    inline void SetSustainLevel(float sus_level) { sus_ = sus_level; }
+    inline void SetSustainLevel(float sus_level)
+    {
+        sus_level = (sus_level <= 0.f) ? -0.01f // forces envelope into idle
+                                       : (sus_level > 1.f) ? 1.f : sus_level;
+        sus_level_ = sus_level;
+    }
     /** get the current envelope segment
         \return the segment of the envelope that the phase is currently located in.
     */
@@ -66,11 +79,19 @@ class Adsr
     inline bool IsRunning() const { return mode_ != ADSR_SEG_IDLE; }
 
   private:
-    float   sus_, seg_time_[ADSR_SEG_LAST], a_, b_, y_, x_, prev_, atk_time_;
+    float   sus_level_{0.f};
+    float   x_{0.f};
+    float   attackShape_{-1.f};
+    float   attackTarget_{0.0f};
+    float   attackTime_{-1.0f};
+    float   decayTime_{-1.0f};
+    float   releaseTime_{-1.0f};
+    float   attackD0_{0.f};
+    float   decayD0_{0.f};
+    float   releaseD0_{0.f};
     int     sample_rate_;
-    uint8_t mode_;
-    float   Tau2Pole(float tau);
-    float   AdsrFilter();
+    uint8_t mode_{ADSR_SEG_IDLE};
+    bool    gate_{false};
 };
 } // namespace daisysp
 #endif
