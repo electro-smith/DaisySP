09a446b08972256918852bd67d30cc6628ed6b15
stephenhensley <stephenhensley@users.noreply.github.com>
2022-09-07 15:52:31 +0000

    deploy: dc448d492e3b8d36173104212b919afd32c976ea

--- search/all_13.js
+++ search/all_13.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['waveforms_0',['Waveforms',['../classdaisysp_1_1_bl_osc.html#abd190108d777a64e9aec2b5508a89b1a',1,'daisysp::BlOsc']]],
-  ['whitenoise_1',['WhiteNoise',['../classdaisysp_1_1_white_noise.html',1,'daisysp']]],
-  ['write_2',['Write',['../classdaisysp_1_1_delay_line.html#a67c4022fe5d1e280e8c0a1b1ec6e8282',1,'daisysp::DelayLine']]],
-  ['write_5fpos_3',['write_pos',['../structdaisysp_1_1_reverb_sc_dl.html#acafadbe36ffcdf81389c196c6486684e',1,'daisysp::ReverbScDl']]]
+  ['wavefolder_0',['Wavefolder',['../classdaisysp_1_1_wavefolder.html',1,'daisysp']]],
+  ['waveforms_1',['Waveforms',['../classdaisysp_1_1_bl_osc.html#abd190108d777a64e9aec2b5508a89b1a',1,'daisysp::BlOsc']]],
+  ['whitenoise_2',['WhiteNoise',['../classdaisysp_1_1_white_noise.html',1,'daisysp']]],
+  ['write_3',['Write',['../classdaisysp_1_1_delay_line.html#a67c4022fe5d1e280e8c0a1b1ec6e8282',1,'daisysp::DelayLine']]],
+  ['write_5fpos_4',['write_pos',['../structdaisysp_1_1_reverb_sc_dl.html#acafadbe36ffcdf81389c196c6486684e',1,'daisysp::ReverbScDl']]]
 ];
--- search/classes_11.js
+++ search/classes_11.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['whitenoise_0',['WhiteNoise',['../classdaisysp_1_1_white_noise.html',1,'daisysp']]]
+  ['wavefolder_0',['Wavefolder',['../classdaisysp_1_1_wavefolder.html',1,'daisysp']]],
+  ['whitenoise_1',['WhiteNoise',['../classdaisysp_1_1_white_noise.html',1,'daisysp']]]
 ];
