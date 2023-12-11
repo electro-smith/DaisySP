6b2f1daaacea023a14d83eec6d679b4a136db6f3
beserge <beserge@users.noreply.github.com>
2023-12-11 20:30:21 +0000

    deploy: 37f4f8b57d0926578533a7bd71e891726699d488

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,8 +1,9 @@
 var searchData=
 [
-  ['fastpower_0',['fastpower',['../namespacedaisysp.html#a207e7a0a645f2db54efb6945e1942ce3',1,'daisysp']]],
-  ['fclamp_1',['fclamp',['../namespacedaisysp.html#a74b876c19ad43dcddf78e89199ecc5ef',1,'daisysp']]],
-  ['fmap_2',['fmap',['../namespacedaisysp.html#ac62832d24f6f6df22fd8858645f4aeba',1,'daisysp']]],
-  ['fmax_3',['fmax',['../namespacedaisysp.html#a9385ec4090ae8925d3e3794312db9396',1,'daisysp']]],
-  ['fonepole_4',['fonepole',['../namespacedaisysp.html#afc3a0874a07573c3149e0c053c11b8d7',1,'daisysp']]]
+  ['fastmod1f_0',['fastmod1f',['../namespacedaisysp.html#a5f0b00de38b7377a333a03d6052662ea',1,'daisysp']]],
+  ['fastpower_1',['fastpower',['../namespacedaisysp.html#a207e7a0a645f2db54efb6945e1942ce3',1,'daisysp']]],
+  ['fclamp_2',['fclamp',['../namespacedaisysp.html#a74b876c19ad43dcddf78e89199ecc5ef',1,'daisysp']]],
+  ['fmap_3',['fmap',['../namespacedaisysp.html#ac62832d24f6f6df22fd8858645f4aeba',1,'daisysp']]],
+  ['fmax_4',['fmax',['../namespacedaisysp.html#a9385ec4090ae8925d3e3794312db9396',1,'daisysp']]],
+  ['fonepole_5',['fonepole',['../namespacedaisysp.html#afc3a0874a07573c3149e0c053c11b8d7',1,'daisysp']]]
 ];
