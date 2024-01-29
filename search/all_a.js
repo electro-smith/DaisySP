ca909d6fd8f6a99ba823e8f12e946634ebb5a682
beserge <beserge@users.noreply.github.com>
2024-01-29 15:06:30 +0000

    deploy: d54d8753a30ac6d265b4be4a49e758a3f5dfb86e

--- search/all_a.js
+++ search/all_a.js
@@ -1,10 +1,11 @@
 var searchData=
 [
-  ['library_20in_20c_0',['DaisySP • A Powerful DSP Library in C++',['../index.html#autotoc_md0',1,'']]],
-  ['license_1',['⚠️ License',['../index.html#autotoc_md8',1,'']]],
-  ['limiter_2',['Limiter',['../classdaisysp_1_1_limiter.html',1,'daisysp']]],
-  ['linearvca_3',['LinearVCA',['../classdaisysp_1_1_linear_v_c_a.html',1,'daisysp']]],
-  ['list_4',['Todo List',['../todo.html',1,'']]],
-  ['looper_5',['Looper',['../classdaisysp_1_1_looper.html',1,'daisysp']]],
-  ['low_6',['Low',['../classdaisysp_1_1_svf.html#a3fbe38989b49fba12a3a7a37f291b33c',1,'daisysp::Svf']]]
+  ['ladderfilter_0',['LadderFilter',['../classdaisysp_1_1_ladder_filter.html',1,'daisysp']]],
+  ['library_20in_20c_1',['DaisySP • A Powerful DSP Library in C++',['../index.html#autotoc_md0',1,'']]],
+  ['license_2',['⚠️ License',['../index.html#autotoc_md8',1,'']]],
+  ['limiter_3',['Limiter',['../classdaisysp_1_1_limiter.html',1,'daisysp']]],
+  ['linearvca_4',['LinearVCA',['../classdaisysp_1_1_linear_v_c_a.html',1,'daisysp']]],
+  ['list_5',['Todo List',['../todo.html',1,'']]],
+  ['looper_6',['Looper',['../classdaisysp_1_1_looper.html',1,'daisysp']]],
+  ['low_7',['Low',['../classdaisysp_1_1_svf.html#a3fbe38989b49fba12a3a7a37f291b33c',1,'daisysp::Svf']]]
 ];
--- search/classes_6.js
+++ search/classes_6.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['limiter_0',['Limiter',['../classdaisysp_1_1_limiter.html',1,'daisysp']]],
-  ['linearvca_1',['LinearVCA',['../classdaisysp_1_1_linear_v_c_a.html',1,'daisysp']]],
-  ['looper_2',['Looper',['../classdaisysp_1_1_looper.html',1,'daisysp']]]
+  ['ladderfilter_0',['LadderFilter',['../classdaisysp_1_1_ladder_filter.html',1,'daisysp']]],
+  ['limiter_1',['Limiter',['../classdaisysp_1_1_limiter.html',1,'daisysp']]],
+  ['linearvca_2',['LinearVCA',['../classdaisysp_1_1_linear_v_c_a.html',1,'daisysp']]],
+  ['looper_3',['Looper',['../classdaisysp_1_1_looper.html',1,'daisysp']]]
 ];
