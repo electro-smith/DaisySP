b00434511a2e3cb9d040f59b6a352ede6d94dc81
Nick Donaldson <nick@infrasonicaudio.com>
2025-03-25 10:46:37 -0600

    Fix coef used in DcBlock
    Reduces aggressive low end rolloff
    and accounts for sample rate

--- Source/Utility/dcblock.cpp
+++ Source/Utility/dcblock.cpp
@@ -7,7 +7,7 @@ void DcBlock::Init(float sample_rate)
 {
     output_ = 0.0;
     input_  = 0.0;
-    gain_   = 0.99;
+    gain_   = expf(-2.0f * M_PI * 10.f / sample_rate);
 }
 
 float DcBlock::Process(float in)
