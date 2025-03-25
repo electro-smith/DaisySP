189076f7a93eb9c0533fb77c1c3ca3ddbd7512b6
Nick Donaldson <nick@infrasonicaudio.com>
2025-03-25 11:18:24 -0600

    Use approximated pole instead of exact

--- Source/Utility/dcblock.cpp
+++ Source/Utility/dcblock.cpp
@@ -7,7 +7,7 @@ void DcBlock::Init(float sample_rate)
 {
     output_ = 0.0;
     input_  = 0.0;
-    gain_   = expf(-2.0f * M_PI * 10.f / sample_rate);
+    gain_   = 1.0 - 10.f / sample_rate;
 }
 
 float DcBlock::Process(float in)
